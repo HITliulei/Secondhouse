46efe911d4301350e7e264460920a29b7d1bbf02
liulei <liulei13041x@163.com>
2019-07-02 14:49:31 +0800

    html1

--- src/main/java/com/ll/secondhouse/test.java
+++ src/main/java/com/ll/secondhouse/test.java
@@ -1,9 +1,13 @@
 package com.ll.secondhouse;
 import com.ll.secondhouse.utils.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class test {
 
 
     public static void main(String[] args) {
-        System.out.println(GetLatAndLngByBaidu.getLngAndLat("哈尔滨市会展中心"));
+//        System.out.println(GetLatAndLngByBaidu.getLngAndLat("哈尔滨市会展中心"));
     }
 }
